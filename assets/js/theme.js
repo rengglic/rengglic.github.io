42872c75e7e2bf1807ee03971716cf54e0d8974a
Cedric Renggli <c_renggli@apple.com>
2025-05-19 13:26:39 +0200

    .

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,9 +3,6 @@
 // Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-// if (themeSetting == "system") {
-//   setThemeSetting("light");
-// } else 
   if (themeSetting == "light") {
     setThemeSetting("dark");
   } else {
@@ -255,12 +252,11 @@ let transTheme = () => {
 // Determine the expected state of the theme toggle, which can be "dark", "light", or
 // "system". Default is "system".
 let determineThemeSetting = () => {
-// let themeSetting = localStorage.getItem("theme");
-// if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-//   themeSetting = "system";
-// }
-// return themeSetting;
-  return "light"
+  let themeSetting = localStorage.getItem("theme");
+  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
+    themeSetting = "light";
+  }
+  return themeSetting;
 };
 
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
