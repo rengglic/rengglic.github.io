02ca4d214bfa85a13b22a6028230b417ef211e60
Cedric Renggli <8517176+rengglic@users.noreply.github.com>
2025-05-19 13:05:26 +0200

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,12 +3,13 @@
 // Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "system") {
-    setThemeSetting("light");
-  } else if (themeSetting == "light") {
+  // if (themeSetting == "system") {
+  //   setThemeSetting("light");
+  // } else 
+  if (themeSetting == "light") {
     setThemeSetting("dark");
   } else {
-    setThemeSetting("system");
+    setThemeSetting("light");
   }
 };
 
@@ -254,11 +255,12 @@ let transTheme = () => {
 // Determine the expected state of the theme toggle, which can be "dark", "light", or
 // "system". Default is "system".
 let determineThemeSetting = () => {
-  let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "system";
-  }
-  return themeSetting;
+  // let themeSetting = localStorage.getItem("theme");
+  // if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
+  //   themeSetting = "system";
+  // }
+  // return themeSetting;
+  return "light"
 };
 
 // Determine the computed theme, which can be "dark" or "light". If the theme setting is
