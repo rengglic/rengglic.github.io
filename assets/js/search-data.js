c34eab1eed51785760b18cc78078277047799af5
rengglic <8517176+rengglic@users.noreply.github.com>
2026-08-31 08:29:59 +0000

    Deploying to gh-pages from @ rengglic/rengglic.github.io@dc71611d65658f3010abded00a8e52ffa4540d27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "news-new-website-is-online",
           title: 'New website is online.',
           description: "",
